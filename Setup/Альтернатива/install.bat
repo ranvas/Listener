8acdd1e2e23fc80543f4e59b274e033357da1447
ranvas <masbox2011@mail.ru>
2015-04-17 15:04:26 +0300

    батник
    внес изменения для обновления библиотеки под windows 8.1

---
+++ Setup/Альтернатива/install.bat
@@ -0,0 +1,5 @@
+::запускать от имени администратора
+cd D:\programming\C#\listener\Listener\ListenerService\bin\Debug
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\InstallUtil.exe ListenerService.exe
+Pause
+Exit
--- UpdateDll/setup.bat
+++ UpdateDll/setup.bat
@@ -2,22 +2,39 @@
 rem ----сохраняем папку с dll в переменную
 set dllName="%~dp0%API.dll"
 if exist %dllName% (
-echo %dllName% exist
+echo API.dll exist
+rem echo %dllName%
 ) else (
-echo %dllName% not exist
+echo API.dll not exist
 goto exit
 )
 rem ----узнаем путь к в реестре к значению пути gacutils
 set regkey = ""
-For /F "UseBackQ Tokens=2*" %%a In (`Reg Query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\MSBuild\ToolsVersions\4.0"^|Find /i "SDK40ToolsPath"`) Do (
+rem на разных версиях ОС по разному
+set query=HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\MSBuild\ToolsVersions\4.0\11.0
+rem set query=HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\MSBuild\ToolsVersions\4.0
+For /F "UseBackQ Tokens=2*" %%a In (`Reg Query "%query%"^|Find /i "SDK40ToolsPath"`) Do (
 set regkey=%%b
 )
-echo %regkey%
+if "%regkey%" == "" (
+echo regkey not exist
+goto exit
+) else (
+echo regkey exist
+rem echo %regkey%
+)
 set regkey=%regkey:~11,-20%
 set folderkey = ""
 For /F "UseBackQ Tokens=2*" %%I In (`Reg Query "%regkey%"^|Find /i "InstallationFolder"`) Do (
 set folderkey=%%J
-echo %%J
+rem echo %%J
+)
+if "%folderkey%" == "" (
+echo folderkey not exist
+goto exit
+) else (
+echo folderkey exist
+rem echo %folderkey%
 )
 
 
